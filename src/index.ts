d674b59d59c92e28827d7aaf2d8f93e39509e36e
Philipp Katz <philipp@philippkatz.de>
2020-06-15 23:39:25 +0200

    Remove obsolete comment

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,6 @@ import { MongoBinary } from 'mongodb-memory-server';
 import { resolve } from 'path';
 import cwd from 'cwd';
 
-// downloads MongoDB which is using by mongodb-memory-server
-// resp. jest-mongodb. Usually this would happen implicitly
-// before running the tests, but we explicitly want to trigger
-// it to make use of the Docker caching
-
 // load the `jest-mongodb-config.js` (copied from node_modules/@shelf/jest-mongodb/setup.js)
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 const { mongodbMemoryServerOptions } = require(resolve(cwd(), 'jest-mongodb-config.js'));
